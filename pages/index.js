fe87545ad4541c1034255f352db93feba659f439
Sandro <sandro.oliveira@rocky.ag>
2022-12-08 09:44:29 -0300

    feat: adds metatags to the project

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
+import { useContext, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/HeroSection'
 import About from '../components/AboutSection'
 import Services from '../components/ServicesSection'
 import styles from '../styles/Home.module.css'
 import Skills from '../components/SkillsSection'
-import Slider from '../components/Slider/Slider'
 import Portfolio from '../components/Portfolio'
-import { Blog } from '../components/BlogSection'
+import {Blog} from '../components/BlogSection'
 import Contact from '../components/Contact'
 import Menu from '../components/Menu'
-import { useContext, useEffect } from 'react'
 import ThemeMenu from '../components/ThemeMenu'
 import AppContext from '../components/AppContext'
 import RadioPlayer from '../components/RadioPlayer'
 import Warning from '../components/Warning'
+import Head from 'next/head'
+import Head_JSX from '../components/Head'
 
 
 export default function Home() {
@@ -37,6 +38,7 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <Head_JSX/>
       <Warning/>
       <Menu/>
       <ThemeMenu/>
