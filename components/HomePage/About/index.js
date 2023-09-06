8ab86021ffcb951da9392d8c027b93de95859db6
Sandro Filho <sandrofilho98@yahoo.com.br>
2023-09-05 23:39:50 -0300

    add new curriculum

--- components/HomePage/About/index.js
+++ components/HomePage/About/index.js
@@ -9,8 +9,8 @@ const About = () => {
 
     const el = useRef(null);
 
-    useEffect(()=>{
-        
+    useEffect(() => {
+
         const typed = new Typed(el.current, {
             strings: [" Web Designer ", " Desenvolvedor Web ", " Programador ", " Freelancer "], // Strings to display
             // Speed settings, try diffrent values untill you get good results
@@ -28,9 +28,9 @@ const About = () => {
         return () => {
             typed.destroy();
         };
-    },[]) 
+    }, [])
 
-    
+
 
 
     return (
@@ -48,13 +48,15 @@ const About = () => {
                     </div>
                     <div className="column right">
                         <div className="text">Eu sou o Sandro Filho e sou <span ref={el} className="typing-2"></span></div>
-                        <p>Nascido e criado no Rio de Janeiro, já busquei minha missão de vida em diversas áreas, porém, foi
-                            na programação onde me encontrei. Linhas de código HTML são o que me movem e transformar isso em
-                            meu trabalho é um sonho se tornando realidade. Programo em ReactJS, NodeJS, Python, VTEXIO e
-                            outras
-                            tecnologias. Busco soluções eficazes e criativas para a resolução do desafio de criar um site
-                            mais moderno e deslumbrante do que o último. Confira abaixo meu portfólio e dê uma olhada em
-                            alguns dos meu trabalhos!</p>
+                        <p>Sou Desenvolvedor Front-End com 3 anos de experiência, fluente em inglês. Comecei minha carreira com Python e agora me especializo em NEXT.js, ReactJS, TypeScript e Tailwind CSS para criar interfaces responsivas e amigáveis ao usuário, sempre utilizando o GIT como ferramenta de controle de versão.
+
+                            Tenho experiência com bancos de dados, incluindo MySQL, Firebase e MongoDB. Além disso, utilizo ferramentas como Figma e Adobe XD para projetar e dar vida às minhas ideias criativas.
+
+                            Atualmente, faço parte da equipe de Otimização de Taxa de Conversão (CRO) na Media.Monks. Lá, combino minhas habilidades técnicas e de design para melhorar a experiência do usuário e impulsionar as conversões.
+
+                            Estou sempre aberto a conexões e oportunidades de colaboração com profissionais talentosos. Se você gostaria de se conectar ou discutir projetos empolgantes, sinta-se à vontade para entrar em contato.
+
+                        </p>
                         <a href="/assets/Curriculum.pdf" target="_blank" rel="noreferrer">Baixar currículo</a>
                     </div>
                 </div>
